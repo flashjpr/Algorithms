3ba9dcbb62e8a06e71065a53a47f1d6b4cf8c94a
flashjpr <flashjpr@gmail.com>
2016-01-20 22:51:56 +0000

    Sum of all primes bellow a certain threshold using the sieve of Eratosthenes

---
+++ sumPrimesBelow2M.py
@@ -0,0 +1,11 @@
+sieve = [True] * 2000000
+
+def sign(sieve, n):
+    for i in range ( 2 * n , len(sieve), n):
+        sieve[i] = False
+
+for i in range (2, int(len(sieve) ** 0.5) + 1):
+    if sieve[i] : sign(sieve, i)
+
+print sum(i for i in range(2, len(sieve)) 
+	if sieve[i])
